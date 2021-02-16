b1ddd249c7f020eefae08c639d35f0d79b4fd18f
debashishere <debashishere007@gmail.com>
2021-02-16 15:47:57 +0530

    password check added

--- middlewares/password.js
+++ middlewares/password.js
@@ -1,13 +1,16 @@
+
+
 module.exports = {
 
     validatePassword: (req, res, next) => {
 
+        if (req.body.password.length < 6) {
+            return res.status(422).send("Passwords length must be greater then 6 character");
+        }
         if (req.body.password != req.body.confromPassword) {
-            return res.json(422, {
-                status: 422,
-                message: 'Passwords did not matched'
-            });
-        } else {
+            return res.status(422).send("Passwords did not matched");
+        }
+        else {
             next();
         }
 
